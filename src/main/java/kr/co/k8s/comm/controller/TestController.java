e3faf5b7d6de36d163b581588def997869f37b53
jinsb <jinsb1999@naver.com>
2023-08-02 15:43:18 +0900

    feat : 기능 추가

--- src/main/java/kr/co/k8s/comm/controller/TestController.java
+++ src/main/java/kr/co/k8s/comm/controller/TestController.java
@@ -10,4 +10,15 @@ public class TestController {
     public String test() {
         return "햇살도 둥글둥글하게 뭉치는 맑은 날";
     }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "동해물과 백두산이 마르고 닳도록";
+    }
+
+    @GetMapping("/test3")
+    public String test3() {
+        return "하느님이 보우하사 우리나라 만세";
+    }
+
 }
